8b7ad9fd7fee745fc30d01823fe516fcfd2e03ea
Gamora00 <almodieljethro16@gmail.com>
2026-03-18 00:14:05 +0800

    fix base

--- src/API/API.jsx
+++ src/API/API.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 
-
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = `resqhavenbackend-production-de5a.up.railway.app`
+//const BASE_URL = 'http://localhost:5000'
 
 
 const api = axios.create({
