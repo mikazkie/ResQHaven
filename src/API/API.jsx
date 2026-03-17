95ac1d4c6dc395d5350e6096e60d3048eb3e09d0
mikazkie <yasuoalmodiel16@gmail.com>
2026-03-18 01:21:37 +0800

    chanaging env

--- src/API/API.jsx
+++ src/API/API.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 
-const BASE_URL =
-  import.meta.env.VITE_API_URL ||
-  'http://localhost:5000'
+
+const BASE_URL = 'https://resqhavenbackend-production-de5a.up.railway.app'
+
+//const BASE_URL = 'http://localhost:5000' 
 
 
 const api = axios.create({
